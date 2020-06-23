f1297b5e0a750bebfd26366ad88e2b09f3851cb2
wiggle131 <jhosiecharlesespantaleon@gmail.com>
2020-06-23 13:00:47 +0800

    "Adding material UI"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Grid from "@material-ui/core/Grid";
 
+let marked = require("marked");
 function App() {
+  const [input, setInput] = useState("");
+
+  function toHtml() {
+    var markUp = marked(input, { sanitizer: true });
+    return { __html: markUp };
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Grid>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <h1>Preview</h1>
+      <div
+        style={{ border: "5px solid black" }}
+        dangerouslySetInnerHTML={toHtml()}
+      ></div>
     </div>
   );
 }
